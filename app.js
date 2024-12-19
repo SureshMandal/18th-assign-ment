4e07000a5d9d3142fec1198690b8e3d51f5199ca
SureshMandal <sureshmandal7989@gmail.com>
2024-12-19 13:49:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// const { john, age } = person;
+// console.log(name); 
+// console.log(age); 
+
+
+
+// let a = 10, b = 20;
+// [a, b] = [b, a];
+// console.log(a); 
+// console.log(b); 
+
+
+// const user = {
+//     details: {
+//         name: 'Alice',
+//         age: 25,
+//         address: {
+//             city: 'New York',
+//             country: 'USA'
+//         }
+//     }
+// };
+
+// const {
+//     details: {
+//         name,
+//         age,
+//         address: { city }
+//     }
+// } = user;
+
+// console.log(name); 
+// console.log(age);  
+// console.log(city);
+
+
+// const users = [
+//     { name: 'John', age: 30 },
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 35 }
+// ];
+
+// const names = users.map(({ name }) => name);
+
+// console.log(names);
+
+
+
+// function getUserInfo({ name, address: { city = 'Unknown' } }) {
+//     return { name, city };
+// }
+
+// const user = {
+//     name: 'John',
+//     address: {
+//         country: 'USA'
+//     }
+// };
+
+// console.log(getUserInfo(user)); 
